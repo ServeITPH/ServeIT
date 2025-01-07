7c287479cde450368c757710802a97c034a89caa
onlyteyl <allenendaya43@gmail.com>
2025-01-08 02:30:22 +0800

    create a navbar

--- index.php
+++ index.php
@@ -10,10 +10,20 @@ include("sharedAssets/connect.php");
     <title>ServeIT | <?php echo $fullName ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
 
+    <!-- nav css -->
+    <link rel="stylesheet" href="assets/css/nav/nav.css">
+
+    <!-- icons -->
+    <script src="https://kit.fontawesome.com/d3ed67e674.js" crossorigin="anonymous"></script>
+
+
+</head>
 </head>
 
 <body>
-
-    <h1>Hello</h1>
+    <?php include("sharedAssets/nav.php");?>
 </body>
---
+++ sharedAssets/nav.php
@@ -0,0 +1,47 @@
+<nav class="navbar navbar-expand-lg ">
+    <div class="container nav-bar-style">
+      <a class="navbar-brand mx-5" href="#"><img src="img-icons/logo-nav.png" style="width: 50px; height: 45px;"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav mx-auto flex-grow-1 justify-content-center">
+          <li class="nav-item mx-3">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item mx-3">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item mx-3">
+            <a class="nav-link" href="#">Product</a>
+          </li>
+          <li class="nav-item mx-3">
+            <a class="nav-link" href="#">Discussion</a>
+          </li>
+          <li class="nav-item mx-3">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item mx-3">
+            <a class="nav-link" href="#">Help center</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto mx-5">
+          <li class="nav-item dropdown mx-3">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Hi, Name<i class="fa-regular fa-user px-2"></i>
+            </a>
+            <ul class="dropdown-menu">
+              <div class="nav-account-title">
+                ACCOUNT
+              </div>
+              <li><a class="dropdown-item" href="#"><i class="fa-solid fa-user nav-icons"></i>Profile</a></li>
+              <li><a class="dropdown-item" href="#"><i class="fa-solid fa-cart-shopping nav-icons"></i>Cart</a></li>
+              <li><a class="dropdown-item" href="#"><i class="fa-solid fa-message nav-icons"></i>Message Us</a></li>
+              <li><a class="dropdown-item" href="#" ><i class="fa-solid fa-right-from-bracket nav-icons"></i>Log-out</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
