396d25c0acc39cb2b42851ecbb8ba68f3587f155
onlyteyl <allenendaya43@gmail.com>
2025-01-10 01:57:52 +0800

    create Landing page, footer and modify other files

---
+++ sharedAssets/footer.php
@@ -0,0 +1,86 @@
+
+
+<!-- help -->
+
+<div class="container-fluid help-section">
+        <div class="help-title">
+            HOW CAN WE HELP?
+        </div>
+        <form class="d-flex justify-content-center align-items-center m-0" role="search">
+            <input class="me-2 need-help" type="search" placeholder="need help?" aria-label="Search">
+            <button class="btn btn-outline-primary need-help-btn" type="submit">Send</button>
+        </form>
+
+    </div>
+
+
+    <!-- main footer -->
+
+    <div class="container-fluid" style="background-color: #000000">
+        <div class="container">
+            <footer class="text-center text-lg-start text-white">
+                <div class="container-fluid p-4 pb-0">
+                    <section class="">
+                        <div class="row">
+                            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                                <h6 class="text-uppercase mb-1" style="font-family:Dela Gothic One, sans-serif;">
+                                    SERVE<span style="color:#19AFA5; ">IT</span>
+                                </h6>
+                                <p style="font-size: 12px;">
+                                    The content on the Serve It website is provided for informational purposes only.
+                                    While
+                                    we strive to ensure accuracy, we do not guarantee the completeness, reliability, or
+                                    timeliness of the information. Use of our website and services is at your own risk.
+                                    Serve It is not liable for any damages or losses resulting from reliance on our
+                                    content
+                                    or services. By using our site, you agree to these terms.
+                                </p>
+                            </div>
+                            <hr class="w-100 clearfix d-md-none" />
+                            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3 text-center">
+                                <h6 class="text-uppercase mb-4 font-weight-bold">MarketPlace</h6>
+                                <p><a class="text-white">Services</a></p>
+                                <p><a class="text-white">Products</a></p>
+                            </div>
+                            <hr class="w-100 clearfix d-md-none" />
+                            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3 text-center">
+                                <h6 class="text-uppercase mb-4 font-weight-bold">COMPANY</h6>
+                                <p><a class="text-white">About ServeIt</a></p>
+                                <p><a class="text-white">Help Center</a></p>
+                                <p><a class="text-white">Contact Us</a></p>
+                            </div>
+                            <hr class="w-100 clearfix d-md-none" />
+                            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3 text-center">
+                                <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
+                                <p><i class="fa-brands fa-tiktok mx-1"></i> ServeIT</p>
+                                <p><i class="fa-brands fa-github mx-1"></i> Serve-IT</p>
+                                <p><i class="fa-brands fa-discord mx-1"></i>Serve-IT.ph</p>
+                            </div>
+                        </div>
+                    </section>
+                    <hr class="my-3">
+                    <section class="p-3 pt-0">
+                        <div class="row d-flex align-items-center">
+                            <div class="col-md-7 col-lg-8 text-center text-md-start">
+                                <div class="p-3">
+                                    © 2024 Copyright:
+                                    <a href="" style="color:#19AFA5">Serve-It.Ph</a>
+                                </div>
+                            </div>
+                            <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+                                <a class="btn btn-outline-light btn-floating m-1" role="button"><i
+                                        class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-outline-light btn-floating m-1" role="button"><i
+                                        class="fab fa-github"></i></a>
+                                <a class="btn btn-outline-light btn-floating m-1" role="button"><i
+                                        class="fab fa-google"></i></a>
+                                <a class="btn btn-outline-light btn-floating m-1" role="button"><i
+                                        class="fab fa-linkedin"></i></a>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </footer>
+        </div>
+    </div>
+
