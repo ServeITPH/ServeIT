7906503043c94488c08f1fd6b4cc8d1def81a2bf
le1nad-prog <melitantedaniel1215@gmail.com>
2025-01-11 00:57:54 +0800

    updated products services page with footer and  modularized css file

--- products.php
+++ products.php
@@ -6,155 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ServeIT | Products</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/nav/nav.css">
     <link rel="icon" href="assets/images/nav/logo-nav.png">
+    <link rel="stylesheet" href="assets/css/product/product.css">
     <link rel="stylesheet" href="assets/css/footer/footer.css">
+    
     <style>
-        * {
-            font-family: 'Poppins', sans-serif;
-            margin: 0;
-            padding: 0;
-        }
-
-        body {
-            margin: 0;
-            padding: 0;
-            justify-content: center;
-            align-items: center;
-            text-align: center;
-        }
-
-        .container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            margin: auto;
-        }
-
-        .card {
-            width: 100%;
-            max-width: 1190px;
-            height: auto;
-            max-height: auto;
-            background-color: black;
-            border-radius: 60px;
-            margin-top: 40px;
-            justify-content: center;
-        }
-
-        .card-title {
-            color: #19AFA5;
-            font-size: 10vh;
-            text-align: center;
-        }
-
-        .card-text {
-            color: white;
-            font-size: 2vh;
-            text-align: justify;
-            padding-left: 10vh;
-            padding-right: 10vh;
-        }
-
-        .search-bar {
-            color: black;
-            background-color: #D2D3D3;
-            border-color: black;
-            width: 625px;
-            max-width: 100%;
-        }
-
-        .filter {
-            color: black;
-            background-color: #D2D3D3;
-            border-color: black;
-            width: 100%;
-            max-width: 300px;
-        }
-
-        .btnApply {
-            width: 100%;
-            max-width: 94px;
-            height: 38px;
-            border-radius: 10px;
-            border: none;
-            color: black;
-            background-color: #19AFA5;
-        }
-
-        .productCard {
-            width: 100%;
-            max-width: 250px;
-            ;
-            height: 378px;
-            background-color: #fff;
-            border-radius: 20px;
-            margin-bottom: 30px;
-        }
-
-        .productImage {
-            width: 100%;
-            height: 250px;
-            background-color: black;
-            border-radius: 23px;
-        }
-
-        .productTitle {
-            font-size: 25px;
-            font-weight: bold;
-        }
-
-        .productPrice {
-            font-size: 20px;
-            font-weight: bold;
-        }
-
-        .productDescription {
-            font-size: 10px;
-        }
-
-        .btnSeeMore {
-            background-color: #19AFA5;
-            width: 100px;
-            height: 25px;
-            border: none;
-            font-size: 12px;
-        }
-
-        .category {
-            text-align: center;
-            font-size: 14px;
-        }
-
-        .pagination {
-            height: 75px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            gap: 5px;
-        }
-
-        .page-item .page-link {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 50px;
-            height: 50px;
-            text-align: center;
-            font-size: 20px;
-            line-height: 50px;
-            border: none;
-            border-radius: 5px;
-            color: black;
-        }
-
-        .page-item .page-link img {
-            width: 45px;
-            height: 45px;
-            object-fit: contain;
-        }
-
         .active3 {
             color: #19AFA5;
         }
@@ -162,8 +21,8 @@
 </head>
 
 <body>
-    <?php include("sharedAssets/nav.php")?>
-    
+    <?php include("sharedAssets/nav.php") ?>
+
     <div class="container mt-5">
         <div class="row">
             <div class="col">
@@ -462,7 +321,7 @@
         </div>
     </div>
 
-    <div class="container pt-5">
+    <div class="container py-5">
         <div class="row">
             <div class="col">
                 <nav aria-label="pageNavigation">
@@ -488,7 +347,11 @@
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <?php include("sharedAssets/footer.php") ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 
 </body>
 
--- services.php
+++ services.php
@@ -6,155 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ServeIT | Services</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/nav/nav.css">
     <link rel="icon" href="assets/images/nav/logo-nav.png">
+    <link rel="stylesheet" href="assets/css/services/services.css">
     <link rel="stylesheet" href="assets/css/footer/footer.css">
+    
     <style>
-        * {
-            font-family: 'Poppins', sans-serif;
-            margin: 0;
-            padding: 0;
-        }
-
-        body {
-            margin: 0;
-            padding: 0;
-            justify-content: center;
-            align-items: center;
-            text-align: center;
-        }
-
-        .container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            margin: auto;
-        }
-
-        .card {
-            width: 100%;
-            max-width: 1190px;
-            height: auto;
-            max-height: auto;
-            background-color: black;
-            border-radius: 60px;
-            margin-top: 40px;
-            justify-content: center;
-        }
-
-        .card-title {
-            color: #19AFA5;
-            font-size: 10vh;
-            text-align: center;
-        }
-
-        .card-text {
-            color: white;
-            font-size: 2vh;
-            text-align: justify;
-            padding-left: 10vh;
-            padding-right: 10vh;
-        }
-
-        .search-bar {
-            color: black;
-            background-color: #D2D3D3;
-            border-color: black;
-            width: 625px;
-            max-width: 100%;
-        }
-
-        .filter {
-            color: black;
-            background-color: #D2D3D3;
-            border-color: black;
-            width: 100%;
-            max-width: 300px;
-        }
-
-        .btnApply {
-            width: 100%;
-            max-width: 94px;
-            height: 38px;
-            border-radius: 10px;
-            border: none;
-            color: black;
-            background-color: #19AFA5;
-        }
-
-        .serviceCard {
-            width: 100%;
-            max-width: 250px;
-            ;
-            height: 378px;
-            background-color: #fff;
-            border-radius: 20px;
-            margin-bottom: 30px;
-        }
-
-        .serviceImage {
-            width: 100%;
-            height: 250px;
-            background-color: black;
-            border-radius: 23px;
-        }
-
-        .serviceTitle {
-            font-size: 25px;
-            font-weight: bold;
-        }
-
-        .servicePrice {
-            font-size: 20px;
-            font-weight: bold;
-        }
-
-        .serviceDescription {
-            font-size: 10px;
-        }
-
-        .btnSeeMore {
-            background-color: #19AFA5;
-            width: 100px;
-            height: 25px;
-            border: none;
-            font-size: 12px;
-        }
-
-        .category {
-            text-align: center;
-            font-size: 14px;
-        }
-
-        .pagination {
-            height: 75px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            gap: 5px;
-        }
-
-        .page-item .page-link {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 50px;
-            height: 50px;
-            text-align: center;
-            font-size: 20px;
-            line-height: 50px;
-            border: none;
-            border-radius: 5px;
-            color: black;
-        }
-
-        .page-item .page-link img {
-            width: 45px;
-            height: 45px;
-            object-fit: contain;
-        }
-
         .active2 {
             color: #19AFA5;
         }
@@ -164,7 +23,7 @@
 <body>
     <?php include("sharedAssets/nav.php") ?>
 
-    <div class="container mt-3">
+    <div class="container mt-5">
         <div class="row">
             <div class="col">
                 <div class="card">
@@ -379,7 +238,7 @@
                 <div class="serviceCard rounded mx-auto">
                     <div class="card-body d-flex flex-column justify-content-between align-items-center">
                         <div class="serviceImage">
-                            <img src="" alt="service Image">
+                            <img src="" alt="Service Image">
                         </div>
                         <div class="w-100 d-flex justify-content-between align-items-center">
                             <span class="serviceTitle">Service Title</span>
@@ -400,7 +259,7 @@
                 <div class="serviceCard rounded mx-auto">
                     <div class="card-body d-flex flex-column justify-content-between align-items-center">
                         <div class="serviceImage">
-                            <img src="" alt="service Image">
+                            <img src="" alt="Service Image">
                         </div>
                         <div class="w-100 d-flex justify-content-between align-items-center">
                             <span class="serviceTitle">Service Title</span>
@@ -421,7 +280,7 @@
                 <div class="serviceCard rounded mx-auto">
                     <div class="card-body d-flex flex-column justify-content-between align-items-center">
                         <div class="serviceImage">
-                            <img src="" alt="service Image">
+                            <img src="" alt="Service Image">
                         </div>
                         <div class="w-100 d-flex justify-content-between align-items-center">
                             <span class="serviceTitle">Service Title</span>
@@ -442,7 +301,7 @@
                 <div class="serviceCard rounded mx-auto">
                     <div class="card-body d-flex flex-column justify-content-between align-items-center">
                         <div class="serviceImage">
-                            <img src="" alt="service Image">
+                            <img src="" alt="Service Image">
                         </div>
                         <div class="w-100 d-flex justify-content-between align-items-center">
                             <span class="serviceTitle">Service Title</span>
@@ -462,21 +321,38 @@
         </div>
     </div>
 
-    <div class="container pt-5">
+    <div class="container py-5">
         <div class="row">
             <div class="col">
-                <button class="search">Search</button>
-            </div>          
+                <nav aria-label="pageNavigation">
+                    <ul class="pagination">
+                        <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Previous">
+                                <img src="assets/images/about/prev.png" alt="Previous">
+                            </a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item"><a class="page-link" href="#">4</a></li>
+                        <li class="page-item"><a class="page-link" href="#">5</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Next">
+                                <img src="assets/images/about/next.png" alt="Next">
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     </div>
 
-     <!-- footer -->
-    <?php include("sharedAssets/smpayment.php") ?>
     <?php include("sharedAssets/footer.php") ?>
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
+
 </body>
 
 </html>
