8cee8befbed94dbd8ed5441ca51206a5d4f006d1
Brandon Mauricio <b24.mauricio@gmail.com>
2025-01-10 18:31:29 +0800

    index.php patch

--- admin/index.php
+++ admin/index.php
@@ -120,7 +120,6 @@
             </div>
         </div>
     </div>
-    <?php include("adminAssets/footer.php") ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
