ba21a15c598dcaf4ad3f05cc1e42bac8202e13a4
le1nad-prog <melitantedaniel1215@gmail.com>
2025-01-11 01:31:19 +0800

    adjusted spacing and css

--- services.php
+++ services.php
@@ -23,7 +23,7 @@
 <body>
     <?php include("sharedAssets/nav.php") ?>
 
-    <div class="container mt-5">
+    <div class="container mt-3">
         <div class="row">
             <div class="col">
                 <div class="card">
