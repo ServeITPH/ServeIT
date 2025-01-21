74abc2f08bc3cfdcef19dcffa32cfec39dab2bba
onlyteyl <allenendaya43@gmail.com>
2025-01-22 00:13:06 +0800

    add new navbar for please log-in

---
+++ sharedAssets/navRegister.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg ">
+  <div class="container nav-bar-style">
+    <a class="navbar-brand mx-5" href="index.php"><img src="assets/images/nav/logo-nav.png"
+        style="width: 50px; height: 45px;"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+      aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav mx-auto flex-grow-1 justify-content-center">
+        <li class="nav-item mx-3">
+          <a class="nav-link active1" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item mx-3">
+          <a class="nav-link active2" href="services.php">Services</a>
+        </li>
+        <li class="nav-item mx-3">
+          <a class="nav-link active3" href="products.php">Products</a>
+        </li>
+        <li class="nav-item mx-3">
+          <a class="nav-link active4" href="about.php">About</a>
+        </li>
+        <li class="nav-item mx-3">
+          <a class="nav-link active5" href="helpCenter.php">Help center</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ms-auto mx-5">
+        <a href="login.php"><button class="btn btn-primary log-in-btn mx-2 mb-2">
+            Log-in / Sign-in
+          </button></a>
+   
+      </ul>
+    </div>
+  </div>
+</nav>
